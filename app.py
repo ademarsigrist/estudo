6886f44838442556b4264380f691d2445fdf4410
Ademar Frederico Nunes Sigrist <ademar.sigrist@gmail.com>
2018-06-06 14:39:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-from flask import Flask, render_template, request
+###################################################################
+# Exemplo de Envio de Email via Flask
+# v1
+# 28/05/2018
+###################################################################
+from flask import Flask, render_template
+from flask_mail import Mail, Message
+import yaml
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-	return render_template("index.html")
+# Dados da caixa postal utilizada para enviar o email
+emailbox = yaml.load(open('email_config.yaml'))
+app.config['MAIL_SERVER']  			= emailbox['eserver']
+app.config['MAIL_PORT']	 			= emailbox['port']
+app.config['MAIL_USE_SSL']			= emailbox['protSSL']
+app.config['MAIL_USE_TLS']  		= emailbox['protTLS']
+app.config['MAIL_USERNAME'] 		= emailbox['username']
+app.config['MAIL_PASSWORD'] 		= emailbox['password']
+app.config['MAIL_DEFAULT_SENDER'] 	= emailbox['fromaddr'] 
 
- 
-if __name__ ==  "__main__":
-	app.run(debug=True)
+mail = Mail(app) 
+
+# Endereço de e-mail destino e mensagem que será enviada
+toaddrs = 'ademar.sigrist@gmail.com'
+subject = 'E-mail Flask'
+txt_msg = 'É Mensagem de teste de envio de email via Flask'
+
+
+# Página index.html
+@app.route('/')
+def index():
+	msg = Message(subject, recipients=[toaddrs])
+	#msg.body = txt_msg 
+	msg.html = "<b>Esse é um teste de HTML</b>"
+	#msg.html = render_template('body_curso_ml.html')
+	#with app.open_resource("teste.txt") as fp:
+	#	msg.attach("teste.txt", "text/plain", fp.read())
+	with app.open_resource("Ficha_Resumo_ML_1.pdf") as fp:
+		msg.attach("Ficha_Resumo_ML_1.pdf", "application/pdf", fp.read())
+	try:
+		mail.send(msg)
+		return "<h1>E-mail é enviado com sucesso!  :) <h1>"
+	except:
+		return 'Ocorreu um ERRO no envio do E-mail!' 
+
+if __name__ == '__main__':
+	app.run(debug=True)  
+
